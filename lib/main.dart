d77e35fbb78e479410ff5fabbc2f07116f1a2b67
DELL <snehal18122004@gmail.com>
2023-02-05 12:01:24 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'nine.dart';
+
+void main()
+{
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: nine(),
+      )
+  );
+}
